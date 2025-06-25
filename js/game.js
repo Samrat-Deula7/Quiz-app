ad6bb90872bb731701b05e2b7f4f93381741fca6
Samrat-Deula7 <samratdeula461@gmail.com>
2025-06-25 10:46:49 +0545

    Adding some questions with some css style and js logic to game.html

---
+++ js/game.js
@@ -0,0 +1,93 @@
+let question =document.getElementById("question");
+const choices =Array.from(document.getElementsByClassName('choice-text'));
+
+let currentQuestion ={};
+let acceptingAnswers=false;
+let score =0;
+let questionCounter=0;
+let availableQuestions=[];
+
+let questions =[
+    {
+        question:"Which country has the larget economy in the world ?",
+        choice1:"chaina",
+        choice2:"India",
+        choice3:"United States",
+        choice4:"New Zealand",
+        answer:3
+    },
+
+    {
+        question:"Who was the runner-up in ICC World Cup 2019 ?",
+        choice1:"United States",
+        choice2:"china",
+        choice3:"New Zealand",
+        choice4:"India",
+        answer: 4
+    },
+        {
+        question:"What does IP stand for ?",
+        choice1:"Internet Protocol",
+        choice2:"Interest Protocol",
+        choice3:"Internet Pass",
+        choice4:"Interest Port",
+        answer:1
+    },
+
+    {
+        question:"Who was the first president of Nepal",
+        choice1:"kp woli",
+        choice2:"Dr. Ram Baran Yadav",
+        choice3:"Dr stone",
+        choice4:"Dr samrat",
+        answer:2
+    },
+]
+
+
+// constants
+
+const correctBonus=10;
+const maxQuestions=3;
+
+getNewQuestion =()=>{
+
+    if(availableQuestions.length == 0 || questionCounter > maxQuestions){
+        return window.location.assign('end.html');
+    }
+    questionCounter ++;
+    const questionIndex= Math.floor(Math.random()*availableQuestions.length);
+    currentQuestion =availableQuestions[questionIndex];
+    question.innerText=currentQuestion.question;
+    
+    choices.forEach(choice => {
+        const number = choice.dataset['number'];
+        choice.innerText=currentQuestion['choice'+number];
+    });
+
+    availableQuestions.splice(questionIndex,1); /*  To remove the question that has just been asked*/
+    acceptingAnswers=true;
+};
+
+choices.forEach(choice=>{
+    choice.addEventListener('click', (e)=>{
+        if(!acceptingAnswers) return;
+
+        acceptingAnswers=false;
+        const selectedChoice=e.target;
+        const selectedAnswer=selectedChoice.dataset['number'];
+        console.log(selectedAnswer);
+        getNewQuestion();
+    });
+})
+
+
+startGame =()=>{
+    questionCounter=0;
+    score=0;
+    availableQuestions=[...questions];
+    getNewQuestion();
+};
+
+
+startGame();
